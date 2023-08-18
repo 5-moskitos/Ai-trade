a3839279658c161b00865595c9e5630e4d3556c8
Sid0698 <siddhantshambharkar@kgpian.iitkgp.ac.in>
2023-08-18 23:29:52 +0530

    con

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -45,17 +45,3 @@ def request_loader(request):
     user = Users.query.filter_by(username=username).first()
     return user if user else None
 
-
-class Transaction(db.Model):
-    __tablename__ = 'Transaction'
-
-    uid=db.Column(db.Integer,db.ForeignKey('Users.id'),nullable=False)
-    tran_id = db.Column(db.Integer, primary_key=True)
-    date_time = db.Column(db.DateTime, nullable=False)
-    Stock_name = db.Column(db.String(64),nullable=False)
-    buySell = db.Column(db.Integer,nullable=False)
-    Price = db.Column(db.Integer,nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-
-
-
